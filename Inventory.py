b90d4770e0a2c6f72acc34b16a89d72c2e69a7fc
Hamid <abdulhamid000@yahoo.com>
2023-12-14 18:59:09 +0000

    first inventory

---
+++ Inventory.py
@@ -0,0 +1,13 @@
+
+
+#Horizon inventory packages
+from user import restpAPI
+from API import *
+
+
+
+def agentInstaller(url, build_number,id,version):
+    printf(f"Called other")
+    #working in process
+
+
